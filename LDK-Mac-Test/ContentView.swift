3debe4018197d5c4194e72feba1508fa8baf16fe
cuhte3 <farid@getportal.co>
2022-06-01 17:48:51 +0400

    basic setup with added LDK framework

---
+++ LDK-Mac-Test/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LDK-Mac-Test
+//
+//  Created by farid on 3/8/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello Lightning Dev Kit")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ LDK-Mac-Test/LDK_Mac_TestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LDK_Mac_TestApp.swift
+//  LDK-Mac-Test
+//
+//  Created by farid on 3/8/22.
+//
+
+import SwiftUI
+
+@main
+struct LDK_Mac_TestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
